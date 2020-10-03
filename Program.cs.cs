399b9aaaf01904fb2dd0c26bea019b16d9c6f730
Gui-Correa <71907354+Gui-Correa@users.noreply.github.com>
2020-10-02 21:11:52 -0300

    Add files via upload
    Programa identifica um valor inteiro e calcula o cobro do mesmo

---
+++ Program.cs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, dobro;
+            Console.WriteLine("Digite um valor inteiro:");
+            valor = Convert.ToInt32(Console.ReadLine());
+
+            dobro = valor + valor;
+
+            Console.WriteLine("O dobro do valor é:" + dobro);
+
+            Console.ReadKey();
+        }
+    }
+}
